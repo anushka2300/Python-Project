ffa834c7a7bbfc446301413ae87e7c00b58f0ed0
anushka2300 <122161237+anushka2300@users.noreply.github.com>
2023-04-28 22:01:40 +0530

    Add files via upload

---
+++ a game of dice.py
@@ -0,0 +1,13 @@
+import random
+print(f'''WELCOME!!!
+RULES:-
+    This is a Game of Dice Where you can role a pair of Dice and get result\n''')
+a=input(f'''To Role a pair of Dice Press \'y\'
+To end this Game Press \'n\'\n''')
+while a=='y':
+    dice1=random.randint(1,6)
+    dice2=random.randint(1,6)
+    print("Your result is",dice1,dice2,"\n")
+    a=input(f'''Do you want to Roll the dice Again then Press \'y\'
+    if you want to end this Game then Press \'n\'\n''')
+print("Game End:))")
